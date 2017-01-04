790ffa42c9a69097597d729e7a19ec181cc30d02
sivanni <simo.vanni@helsinki.fi>
2017-01-04 08:06:20 +0200

    Building synaptic scaling, version 0.2

--- data_visualizers/data_visualization.py
+++ data_visualizers/data_visualization.py
@@ -8,7 +8,7 @@ from brian2 import *
 import bz2
 import pandas as pd
 
-time_for_visualization = np.array([0, 9.99])   #+ 0.00001  # To accept 0 as starting point. Rounding error for the end.
+time_for_visualization = np.array([0, 0.09])   #+ 0.00001  # To accept 0 as starting point. Rounding error for the end.
 # dt = 0.1 * ms
 plot_dt = 1 * ms
 
